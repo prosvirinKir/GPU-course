dde99bd9121cb4fc705c4768fec4606881f317fa
KirillProsvirin <kirill.prosvirin@gmail.com>
2023-02-07 17:55:26 +0100

    add simple sphere

--- ray tracer/main.cpp
+++ ray tracer/main.cpp
@@ -4,9 +4,23 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
+
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
@@ -15,7 +29,7 @@ int main() {
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 1024;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
@@ -41,7 +55,7 @@ int main() {
             // auto b = 0.25;
 
             // color pixel_color(r, g, b);
-          
+
             auto u = double(i) / (image_width-1);
             auto v = double(j) / (image_height-1);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
